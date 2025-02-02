fd5f41b50d3ee3dbe9eedf7bce2246b78c0c6fb0
Zouhair Begdar <parkour@ora.ma>
2025-02-02 03:08:47 +0100

    add custom back button

---
+++ components/BackButton.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { BackButtonProps } from '@/types'
+import { useRouter } from 'expo-router';
+import { Car, CaretLeft } from 'phosphor-react-native';
+import { verticalScale } from '@/utils/styling';
+import { colors, radius } from '@/constants/theme';
+
+const BackButton = ({ style, iconSize = 26 }: BackButtonProps
+) => {
+	const router = useRouter();
+	return (
+		<TouchableOpacity style={[styles.backButton, style]} onPress={() => router.back()}>
+			<CaretLeft size={verticalScale(iconSize)} color={colors.white} weight='bold' />
+		</TouchableOpacity>
+	)
+}
+
+export default BackButton
+
+const styles = StyleSheet.create({
+	backButton: {
+		backgroundColor: colors.neutral600,
+		alignSelf: 'flex-start',
+		borderRadius: radius._12,
+		borderCurve: 'continuous',
+		padding: verticalScale(5),
+	}
+})
