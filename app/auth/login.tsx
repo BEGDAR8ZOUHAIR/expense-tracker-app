fb28ba9e9b3f36c894159b53d764bcf5226fcaf4
Zouhair Begdar <parkour@ora.ma>
2025-02-02 12:35:26 +0100

    add input componnent and complet login screen

---
+++ app/auth/login.tsx
@@ -0,0 +1,103 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import { colors, spacingX, spacingY } from '@/constants/theme'
+import { Mailbox, Lock } from 'phosphor-react-native';
+import { verticalScale } from '@/utils/styling'
+import BackButton from '@/components/BackButton'
+import Typo from '@/components/Typo'
+import Input from '@/components/Input'
+import Button from '@/components/Button'
+import { Link, useRouter } from 'expo-router'
+
+const login = () => {
+	const emailRef = React.useRef('');
+	const passordRef = React.useRef('');
+	const [loading, setLoading] = React.useState(false);
+	const router = useRouter();
+	const handleSumbit = () => {
+
+	}
+
+	return (
+		<ScreenWrapper>
+			<View style={styles.container}>
+				{/* back button	 */}
+				<BackButton iconSize={28} />
+
+				<View style={{ gap: 5, marginTop: spacingY._20 }}>
+					<Typo fontWeight={'700'} size={24}>Sign In</Typo>
+					<Typo fontWeight={'500'}>Enter your email and password</Typo>
+				</View>
+				{/* form */}
+				<View style={styles.form}>
+					<Input
+						icon={<Mailbox color={colors.neutral300} />}
+						placeholder='Enter your email'
+						onChangeText={(value) => (emailRef.current = value)}
+					/>
+					<Input
+						icon={<Lock color={colors.neutral300} />}
+						placeholder='Enter your password'
+						secureTextEntry
+						onChangeText={(value) => (passordRef.current = value)}
+					/>
+				</View>
+				{/* forgot password */}
+				<Text style={styles.forgotPassword}>Forgot Password?</Text>
+				{/* button */}
+				<Button onPress={handleSumbit} loading={loading}>
+					<Typo size={verticalScale(16)} fontWeight={'500'} color={colors.neutral900}>Login</Typo>
+				</Button>
+
+				{/* footer */}
+				<View style={styles.footer}>
+					<Link href={'/auth/register'}>
+						<Text style={styles.footerText}>Don't have an account? {' '}</Text>
+						<Text style={styles.footerLinkText}>Register</Text>
+					</Link>
+				</View>
+			</View>
+		</ScreenWrapper >
+	)
+}
+
+export default login
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		gap: spacingY._30,
+		paddingHorizontal: spacingX._20,
+		fontWeight: '700',
+		color: colors.text
+	},
+	welcomeText: {
+		fontSize: verticalScale(20),
+		fontWeight: '700',
+		color: colors.text
+	},
+	form: {
+		gap: spacingY._20
+	},
+	forgotPassword: {
+		textAlign: 'right',
+		fontWeight: '500',
+		color: colors.text
+	},
+	footer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 5
+	},
+	footerText: {
+		textAlign: 'center',
+		color: colors.text,
+		fontSize: verticalScale(14)
+	},
+	footerLinkText: {
+		color: colors.primary,
+		fontSize: verticalScale(14),
+	}
+})
--- app/auth/welcome.tsx
+++ app/auth/welcome.tsx
@@ -6,52 +6,55 @@ import { colors, spacingX, spacingY } from '@/constants/theme'
 import { verticalScale } from '@/utils/styling'
 import Button from '@/components/Button'
 import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated'
+import { useRouter } from 'expo-router'
 
 
-const welcome = () => (
-	<ScreenWrapper>
-		<View style={styles.container}>
-			{/* login */}
-			<View>
-				<TouchableOpacity style={styles.loginButton}>
+const welcome = () => {
+	const router = useRouter();
+	return (
 
-					<Typo fontWeight={'500'}>Sign In</Typo>
+		<ScreenWrapper>
+			<View style={styles.container}>
+				{/* login */}
+				<View>
+					<TouchableOpacity style={styles.loginButton} onPress={() => router.push('/auth/login')}>
+						<Typo fontWeight={'500'}>Sign In</Typo>
+					</TouchableOpacity>
+					<Animated.Image
+						entering={FadeIn.duration(1000)}
+						source={require('../../assets/images/welcome.png')}
+						style={styles.welcomeImage}
+						resizeMode='contain'
+					/>
+				</View>
 
-				</TouchableOpacity>
-				<Animated.Image
-					entering={FadeIn.duration(1000)}
-					source={require('../../assets/images/welcome.png')}
-					style={styles.welcomeImage}
-					resizeMode='contain'
-				/>
+				{/* footer */}
+				<View style={styles.footer}>
+					<Animated.View
+						entering={FadeInDown.duration(1000).springify().damping(12)} style={{ alignItems: 'center' }}
+					>
+						<Typo fontWeight={'700'} size={24}>Always take control</Typo>
+						<Typo fontWeight={'700'} size={24}>of your finances</Typo>
+					</Animated.View>
+					<Animated.View
+						entering={FadeInDown.duration(1000).delay(500).springify().damping(12)} style={{ alignItems: 'center', gap: 2 }}
+					>
+						<Typo size={16}>Manage your finances with ease</Typo>
+						<Typo size={16}>track your spending habits</Typo>
+					</Animated.View>
+
+					{/* button */}
+					<Animated.View style={styles.buttonContainer} entering={FadeInDown.duration(1000).delay(200).springify().damping(12)}>
+						<Button onPress={() => { router.push('/auth/register') }}>
+							<Typo size={22} fontWeight={'600'} color={colors.neutral900}>Get Started</Typo>
+						</Button>
+					</Animated.View>
+				</View>
 			</View>
+		</ScreenWrapper >
 
-			{/* footer */}
-			<View style={styles.footer}>
-				<Animated.View
-					entering={FadeInDown.duration(1000).springify().damping(12)} style={{ alignItems: 'center' }}
-				>
-					<Typo fontWeight={'700'} size={24}>Always take control</Typo>
-					<Typo fontWeight={'700'} size={24}>of your finances</Typo>
-				</Animated.View>
-				<Animated.View
-					entering={FadeInDown.duration(1000).delay(500).springify().damping(12)} style={{ alignItems: 'center', gap: 2 }}
-				>
-					<Typo size={16}>Manage your finances with ease</Typo>
-					<Typo size={16}>track your spending habits</Typo>
-				</Animated.View>
-
-				{/* button */}
-				<Animated.View style={styles.buttonContainer} entering={FadeInDown.duration(1000).delay(200).springify().damping(12)}>
-					<Button onPress={() => { }}>
-						<Typo size={22} fontWeight={'600'} color={colors.neutral900}>Get Started</Typo>
-					</Button>
-				</Animated.View>
-			</View>
-		</View>
-	</ScreenWrapper >
-
-)
+	)
+}
 
 export default welcome
 
