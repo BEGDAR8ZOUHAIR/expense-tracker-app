2c1b46b6edeee80bc6245716226f4d81874e7921
Zouhair Begdar <parkour@ora.ma>
2025-02-02 14:50:01 +0100

    add auth context and setup firebase

---
+++ config/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "@firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDudpuyYFuEOdMczPdIT2kwSVIEEb4fTeI",
+	authDomain: "expense-tracker-c3678.firebaseapp.com",
+	projectId: "expense-tracker-c3678",
+	storageBucket: "expense-tracker-c3678.firebasestorage.app",
+	messagingSenderId: "364849923301",
+	appId: "1:364849923301:web:22b8137091013108e1400c",
+	measurementId: "G-86GFWZXRNF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// auth
+export const auth = initializeAuth(app, {
+	persistence: getReactNativePersistence(AsyncStorage)
+});
+
+// database
+
+export const firestore = getFirestore(app);
---
+++ contexts/authContext.tsx
@@ -0,0 +1,90 @@
+import { createContext, useContext, useState } from 'react';
+import { AuthContextType, UserType } from '@/types';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, firestore } from '@/config/firebase';
+import { doc, getDoc, setDoc } from '@firebase/firestore';
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+	const [user, setUser] = useState<UserType>(null)
+
+	// functions for auth
+	// login
+	const login = async (email: string, password: string) => {
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
+			return { success: true }
+		} catch (error: any) {
+			let msg = error.message;
+			return { success: false, msg }
+		}
+	}
+
+	//  register
+	const register = async (email: string, password: string, name: string) => {
+		try {
+			const response = await createUserWithEmailAndPassword(auth, email, password);
+			await setDoc(doc(firestore, 'users', response?.user?.uid),
+				{
+					name,
+					email,
+					uid: response?.user?.uid
+				}
+			);
+			return { success: true }
+
+		} catch (error: any) {
+			let msg = error.message;
+			return { success: false, msg }
+		}
+	}
+
+	// update user data
+	const updateUserData = async (uid: string) => {
+		try {
+			const docRef = doc(firestore, 'users', uid);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				const data = docSnap.data();
+				const userData: UserType = {
+					uid: data?.uid,
+					email: data?.email || null,
+					name: data?.name || null,
+					image: data?.image || null
+
+				}
+				setUser(userData);
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+			}
+		} catch (error) {
+			console.error("Error fetching user data:", error);
+		}
+	}
+
+	const contextValue: AuthContextType = {
+		user,
+		setUser,
+		login,
+		register,
+		updateUserData
+	}
+
+	return (
+		<AuthContext.Provider value={contextValue}>
+			{children}
+		</AuthContext.Provider>
+	)
+
+}
+
+
+export const useAuth = (): AuthContextType => {
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error('useAuth must be used within a AuthProvider');
+	}
+	return context;
+};
